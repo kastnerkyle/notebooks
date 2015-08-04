5cee3c90b6c84b9b57f5f3f56cf4f0ac5c0a0fdd
Kyle Kastner <kastnerkyle@gmail.com>
2015-08-04 07:44:22 -0400

    Added notebooks

---
+++ cleanup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+rm *.zip
+rm *.mat
+rm *.webm
+rm *.mp4
+rm *.csv
+rm *.gif
+rm *.gz
+rm *.tar
+rm *.pyc
+rm -rf Campus Curtain Bootstrap hall ShoppingMall WaterSurface Fountain Escalator
+rm -rf an4 audio pages
+rm -rf .ropeproject
---
+++ utils.py
@@ -0,0 +1,24 @@
+import os
+import urllib2
+
+def progress_bar_downloader(url, fname, progress_update_every=5):
+    #from http://stackoverflow.com/questions/22676/how-do-i-download-a-file-over-http-using-python/22776#22776
+    u = urllib2.urlopen(url)
+    f = open(fname, 'wb')
+    meta = u.info()
+    file_size = int(meta.getheaders("Content-Length")[0])
+    print "Downloading: %s Bytes: %s" % (fname, file_size)
+    file_size_dl = 0
+    block_sz = 8192
+    p = 0
+    while True:
+        buffer = u.read(block_sz)
+        if not buffer:
+            break
+        file_size_dl += len(buffer)
+        f.write(buffer)
+        if (file_size_dl * 100. / file_size) > p:
+            status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+            print status
+            p += progress_update_every
+    f.close()
